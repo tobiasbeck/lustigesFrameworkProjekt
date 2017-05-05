573cd463677b92d73ff80f422ab62862add24e76
supertobs <tob.beck@icloud.com>
2017-05-05 11:26:04 +0200

    Added map + disabled eslint

---
+++ lustig/src/components/Map.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+<h1>Find your grill on our grilling map</h1>
+ <gmap-map
+    :center="center"
+    :zoom="7"
+    style="width: 500px; height: 300px"
+  ></gmap-map>
+  </div>
+</template>
+
+<script>
+import * as VueGoogleMaps from 'vue2-google-maps';
+  import Vue from 'vue';
+ 
+  Vue.use(VueGoogleMaps, {
+    load: {
+      key: 'AIzaSyA1OUPKyuJM6J3_IH6IYwpYhMLOK73bGBo',
+      v: '',
+      // libraries: 'places', //// If you need to use place input 
+    }
+  });
+
+  export default {
+    data () {
+      return {
+        center: {lat: 10.0, lng: 10.0},
+        markers: [{
+          position: {lat: 10.0, lng: 10.0}
+        }, {
+          position: {lat: 11.0, lng: 11.0}
+        }]
+      }
+    }
+  }
+</script>
